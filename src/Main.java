23000d00956690ed76227687a80766f61704ff52
Дугаров Вячеслав <sdugharow@yandex.ru>
2022-09-20 16:58:12 +0800

    text for commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
+    List<Integer> array = new ArrayList<>();
+    Random generator = new Random();
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //TODO Задача 1. Сгенерируйте массив целых чисел, используя класс Random(). Используя методы классов-коллекций определите уникальные числа (вариант 2)
